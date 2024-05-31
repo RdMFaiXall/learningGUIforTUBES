2389c6735d0fee0ba83dce7b34b3cf104df1b46e
RD FAISAL <xallgitautopilot0@gmail.com>
2024-05-31 17:14:13 +0700

    first commit

---
+++ LearnGUI-Tubes/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace LearnGUI_Tubes
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            label8 = new Label();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(114, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(147, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Nama Produk";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Narrow", 20.25F);
+            label2.Location = new Point(114, 128);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 31);
+            label2.TabIndex = 1;
+            label2.Text = "Tipe Produk";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Narrow", 20.25F);
+            label3.Location = new Point(114, 181);
+            label3.Name = "label3";
+            label3.Size = new Size(148, 31);
+            label3.TabIndex = 2;
+            label3.Text = "Harga Produk";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial Narrow", 20.25F);
+            label4.Location = new Point(114, 230);
+            label4.Name = "label4";
+            label4.Size = new Size(169, 31);
+            label4.TabIndex = 3;
+            label4.Text = "Panjang Produk";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial Narrow", 20.25F);
+            label5.Location = new Point(114, 279);
+            label5.Name = "label5";
+            label5.Size = new Size(144, 31);
+            label5.TabIndex = 4;
+            label5.Text = "Lebar Produk";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial Narrow", 20.25F);
+            label6.Location = new Point(114, 329);
+            label6.Name = "label6";
+            label6.Size = new Size(133, 31);
+            label6.TabIndex = 5;
+            label6.Text = "Stok Produk";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial Narrow", 20.25F);
+            label7.Location = new Point(114, 379);
+            label7.Name = "label7";
+            label7.Size = new Size(179, 31);
+            label7.TabIndex = 6;
+            label7.Text = "Deskripsi Produk";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(357, 81);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(469, 29);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(357, 129);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(469, 29);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(357, 182);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(469, 29);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(357, 232);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(469, 29);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(357, 281);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(469, 29);
+            textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox6.Location = new Point(357, 331);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(469, 29);
+            textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox7.Location = new Point(357, 381);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(469, 29);
+            textBox7.TabIndex = 13;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Arial Narrow", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ImageAlign = ContentAlignment.TopCenter;
+            label8.Location = new Point(309, 28);
+            label8.Name = "label8";
+            label8.Size = new Size(314, 31);
+            label8.TabIndex = 14;
+            label8.Text = "Masukkan Data Produk Baru";
+            label8.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial", 15.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.Location = new Point(419, 437);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 38);
+            button1.TabIndex = 15;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.LinkColor = Color.Black;
+            linkLabel1.Location = new Point(11, 10);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(56, 21);
+            linkLabel1.TabIndex = 16;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Cancel";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(944, 501);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(label8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Label label8;
+        private Button button1;
+        private LinkLabel linkLabel1;
+    }
+}
---
+++ LearnGUI-Tubes/Form1.cs
@@ -0,0 +1,30 @@
+namespace LearnGUI_Tubes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
